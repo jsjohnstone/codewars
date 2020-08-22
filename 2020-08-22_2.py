ebac071abd330a5607f74200205759e2e07a0ad5
Jamie Johnstone <jamie@tixy.co.nz>
2020-08-22 20:38:14 +0100

    Update formatting of 2nd kata

--- 2020-08-22_2.py
+++ 2020-08-22_2.py
@@ -1,16 +1,26 @@
 # Printer Errors
 # https://www.codewars.com/kata/56541980fa08ab47a0000040/train/python
+#
+# Examples:
+# s="aaabbxbhaijjjm"
+# printer_error(s) => "1/14"
 
 def printer_error(s):
+
+    # define some important variables...
     total = len(s)
     i = 0
     err = 0
-    
+
+    # iteration through characters of the string
     while i < total:
+
+        # if one of those characters is not in the allowed list, add an error count
         if s[i] not in "abcdefghijklm":
             err += 1
         i += 1
     
+    # format the output as required - num errors / total characters
     result = "{}/{}".format(err, total)
     
     return result
